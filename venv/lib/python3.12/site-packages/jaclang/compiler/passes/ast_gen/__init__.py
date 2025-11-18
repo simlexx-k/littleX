6c0c8d24ba4a7d295db1d7bb660b90751ebc3580
Simon Kosgei <simonwriter254@gmail.com>
2025-11-18 20:20:42 +0300

    Ignore virtualenv

--- venv/lib/python3.12/site-packages/jaclang/compiler/passes/ast_gen/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Common helpers for AST generation passes."""
-
-from .base_ast_gen_pass import BaseAstGenPass
-
-__all__ = ["BaseAstGenPass"]
--- venv/lib/python3.12/site-packages/jaclang/tests/fixtures/funccall_genexpr.py
+++
@@ -1,5 +0,0 @@
-def total(values):
-    return sum(values)
-
-result = total((x * x) for x in range(5))
-print(result) 
